bdef582b99df0870b13c58f952af519aa754554f
PabloRC0525 <121038827@upq.edu.mx>
2023-07-04 18:55:43 -0600

    Se agregaron las funciones de editar y eliminar album

--- Practica 5/app.py
+++ Practica 5/app.py
@@ -38,9 +38,47 @@ def guardar():
     flash('Album Agregado Correctamente :)')
     return redirect(url_for('index'))
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se eliminó el album en la BD"
+@app.route('/editar/<id>')
+def editar(id):
+    cureditar = mysql.connection.cursor()
+    cureditar.execute('select * from albums where id = %s',(id,))
+    consulId = cureditar.fetchone()
+    
+    return render_template('editarAlbum.html', consulId=consulId)
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+        
+        CS = mysql.connection.cursor()
+        CS.execute('update albums set titulo = %s, artista = %s, anio = %s where id = %s',(Vtitulo,Vartista,Vanio,id))
+        mysql.connection.commit()
+        
+    flash('Album Actualizado en BD :)')
+    return redirect(url_for('index'))
+
+@app.route('/eliminar/<id>')
+def eliminar(id):
+    cureditar = mysql.connection.cursor()
+    cureditar.execute('select * from albums where id = %s',(id,))
+    consulId = cureditar.fetchone()
+    
+    return render_template('eliminarAlbum.html', consulId=consulId)
+
+@app.route('/eliminaralb/<id>', methods=['POST'])
+def eliminaralb(id):
+    if request.method == 'POST':
+        
+        CS = mysql.connection.cursor()
+        CS.execute('delete from albums  where id = %s',(id,))
+        mysql.connection.commit()
+        
+    flash('Album Eliminado en BD :)')
+    return redirect(url_for('index'))
 #Ejecucion de servidor
 if __name__ =='__main__':
     app.run(port=5000,debug=True)
+
